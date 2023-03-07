d6467538fd7d4c346502c7f4401f6cf4ec09d994
Trinta <trintoni@gmail.com>
2023-03-07 13:55:08 -0300

    teste

--- goodbye.sh
+++ goodbye.sh
@@ -1,1 +1,17 @@
 echo "Goodbye Bitch"
+
+if [ ! -z "$name_aks_cluster" ];then
+    touch /tmp/teste_aks
+fi
+
+AKS_SETTINGS_FILE=""
+if [[ -f "aks.settings.yml" ]]; then
+  AKS_SETTINGS_FILE="aks.settings.yml"
+elif [[ -f "aks.settings.yaml" ]]; then
+  AKS_SETTINGS_FILE="aks.settings.yaml"
+else
+  echo -e "\n#############################################"
+  echo "Missing file aks.settings.yml for sandbox deployment"
+  echo -e "#############################################\n"
+  exit 1
+fi
